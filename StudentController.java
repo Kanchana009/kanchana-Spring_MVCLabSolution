a0281ebec5ff6cb8a516acbb12d73aa16463d70a
Kanchana009 <87772476+Kanchana009@users.noreply.github.com>
2021-12-12 17:09:03 +0530

    Update StudentController.java

--- StudentController.java
+++ StudentController.java
@@ -1,4 +1,5 @@
-package com.gl.SpringProject.controller;
+package com.collegefest.controller;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.gl.SpringProject.entity.Student;
-import com.gl.SpringProject.service.StudentService;
+import com.collegefest.entity.Student;
+import com.collegefest.service.StudentService;
 
 @Controller
 @RequestMapping("/students")
@@ -17,29 +18,29 @@ public class StudentController {
 
 	@Autowired
 	private StudentService studentService;
-	
+
 	@RequestMapping("/list")
 	public String listStudents(Model model) {
 		List<Student> students = studentService.findAll();
-		
+
 		model.addAttribute("Students", students);
-		
+
 		return "list-Students";
 	}
-	
+
 	@RequestMapping("/showFormForAdd")
-	public String showFormForAdd( Model theModel) {
+	public String showFormForAdd(Model theModel) {
 
 		// get the Student from the service
-		
-       Student student = new Student();
+
+		Student student = new Student();
 		// set Student as a model attribute to pre-populate the form
 		theModel.addAttribute("Student", student);
 
 		// send over to our form
 		return "Student-form";
 	}
-	
+
 	@RequestMapping("/showFormForUpdate")
 	public String showFormForUpdate(@RequestParam("studentId") int theId, Model theModel) {
 
@@ -51,10 +52,8 @@ public class StudentController {
 
 		// send over to our form
 		return "Student-form";
-			
 	}
-	
-	
+
 	@PostMapping("/save")
 	public String saveStudent(@RequestParam("id") int id, @RequestParam("name") String name,
 			@RequestParam("department") String department, @RequestParam("country") String country) {
@@ -75,7 +74,7 @@ public class StudentController {
 		return "redirect:/students/list";
 
 	}
-	
+
 	@RequestMapping("/delete")
 	public String delete(@RequestParam("studentId") int theId) {
 
@@ -86,7 +85,5 @@ public class StudentController {
 		return "redirect:/students/list";
 
 	}
-	
-	
-	
-	}
+
+}
